49b07b4b662f560207cd58f9d9a8b43469769ac7
“nash1124” <nash1596315963@gmail.com>
2023-02-04 15:07:06 +0800

    0204

--- src/App.js
+++ src/App.js
@@ -1,23 +1,115 @@
-import logo from './logo.svg';
+import { useState , useEffect, useCallback } from 'react';
 import './App.css';
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width : 80% ;
+  min-width : 300px;
+  min-height : 300px;
+  display : flex;
+  border : 1px solid black;
+  margin: 20px auto ;
+  flex-wrap: wrap ;
+  justify-content: flex-start ;
+`
+
+export const imgStyle ={
+  width : '100%',
+  height : '90%',
+}
+
+export const buttonStyle ={
+  width : '90%' ,
+  margin : '5px auto'
+}
 
 function App() {
+  const [list, setList] = useState([]);
+  const [like, setLike] = useState([]);
+  const [count, setCount] = useState(0);
+
+  const url = "https://api.themoviedb.org/3/movie/top_rated?api_key=7dcd0053886fc8ec5f0fc4ee25fa55a1&language=zh-TW&page=1";
+  
+  const handleFetchData = (url) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((myJson) => {
+        // console.log(myJson);
+        setList(
+          myJson.results.map((ele) => {
+            return {
+              id: ele.id,
+              title: ele.title ? ele.title : ele.original_title,
+              posterUrl: `https://image.tmdb.org/t/p/w500${ele.poster_path}`,
+            };
+          })
+        );
+      })
+      .catch((err) => console.log(err));
+  };
+  const handleLikeData=()=>{
+    const state = JSON.parse(localStorage.getItem('list')) ;
+    // console.log(state)
+    if(state.length >0){
+      setLike([...state])
+    }
+  }
+  
+  const handleMovieClick =(obj)=>{
+    const item = localStorage.getItem('list') ? true : false;
+    if(!item){
+      console.log(obj)
+      localStorage.setItem('list', JSON.stringify([obj]))
+      setCount((pre)=>pre+1)
+    }
+    else{
+      const data = JSON.parse(localStorage.getItem('list'))
+      data.push(obj)
+      localStorage.setItem('list',JSON.stringify(data))
+      setCount((pre)=>pre+1)
+    }
+  }
+  const handleDelete = (id) => {
+    setCount((pre)=>pre-1)
+    const data = JSON.parse(localStorage.getItem('list'))
+    const index = data.findIndex((ele)=>ele.id === id)
+    data.splice(index,1)
+    localStorage.setItem('list',JSON.stringify(data))
+    
+  }
+
+  useEffect(()=>{
+    handleFetchData(url)
+  },[])
+  
+
+  useEffect(()=>{
+    handleLikeData()
+  },[count])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>movie list</h1>
+      <Container>
+        {list.map((item) => {
+          return (
+            <div key={item.id} className='movie'>
+              <img src={item.posterUrl} alt={item.originalTitle} style={imgStyle}/>
+              <button type="vlaue" style={buttonStyle} onClick={()=>{handleMovieClick(item)}}>Get</button>
+            </div>
+        )})}
+      </Container>
+      <h1>like list</h1>
+      <Container>
+      {like.map((item)=> {
+        return(
+          <div key={item.id} className='movie'>
+              <img src={item.posterUrl} alt={item.originalTitle} style={imgStyle}/>
+              <button type="vlaue" style={buttonStyle} onClick={()=>{handleDelete(item.id)}}>delete</button>
+          </div>
+        )
+      })}
+      </Container>
     </div>
   );
 }
